184620873506c28b1bcabe92776e096dd6383c7c
Ivan Chirkin <chirkin.ivan@gmail.com>
2024-05-08 12:18:36 +0300

    init

---
+++ Formula/texp.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Texp < Formula
+  desc "texp is a small tool for render templates"
+  homepage "https://github.com/fixcik/texp"
+  url "https://github.com/fixcik/texp/releases/download/v0.2.3/texp-x86_64-apple-darwin.tar.gz"
+  sha256 "6213e274fb7df9e47e9ad5d7eefa9b67f35074b6d8585665fce82b8d34233a77"
+  license "GPL-3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test texp`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
