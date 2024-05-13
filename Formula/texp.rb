38d70bbfbea8645f52426bbec383bc8248fafda8
Ivan Chirkin <chirkin.ivan@gmail.com>
2024-05-13 14:40:08 +0300

    update

--- Formula/texp.rb
+++ Formula/texp.rb
@@ -1,8 +1,8 @@
 class Texp < Formula
   desc "Small tool for render templates"
   homepage "https://github.com/fixcik/texp"
-  url "https://github.com/fixcik/texp/releases/download/v0.2.3/texp-x86_64-apple-darwin.tar.gz"
-  sha256 "6213e274fb7df9e47e9ad5d7eefa9b67f35074b6d8585665fce82b8d34233a77"
+  url "https://github.com/fixcik/texp/releases/download/v0.2.4/texp-x86_64-apple-darwin.tar.gz"
+  sha256 "91b7900f8a10c71e753a795925875d9e1840c046215d926b1d0ee1cb661e491b"
   license "GPL-3.0-or-later"
 
   def install
